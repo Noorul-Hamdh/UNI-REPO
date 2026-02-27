3d57745985ae115e5c64c13150cc008cfb3dabf3
Noorul-Hamdh <noorulhamdh@gmail.com>
2026-02-27 08:01:52 +0530

    modified:   pba1.cpp 	new file:   qb.cpp

--- pba1.cpp
+++ pba1.cpp
@@ -170,6 +170,7 @@ using namespace std;
 //             if(node==nullptr){
 //                 return new Node(val);
 //             }
+//         }
 //             if(val<node->data){
 //                 node->left = insert(node->left,val);
 //             }else{
@@ -178,12 +179,55 @@ using namespace std;
 //             return node;
 //         }
 
+//         Node* search(Node* node,int val){
+//             if(node==nullptr)return false;
+//             if(node->data==val)return true;
+//             if(node->data > val){
+//                 return search(node->left,val);
+//             }
+//             return search(node->right,val);
+//         }
 
-//         void inorder(){
+
+//         void inorder(Node* node){
 //             inOrder(node->left);
 //             cout<<node->data;
 //             inOrder(node->right);
 //         }
+
+//         void printrange(Node* node,int l,int r){
+//             if(node==nullptr){
+//                 return 0;
+//             }
+//             if(node->data>l){
+//                 printrange(node->left,l,r)
+//             }
+//             if(node->data>=l && node->data<=r){
+//                 cout<<node->data;
+//             }
+//             if(node->data<r){
+//                 printrange(node->right,l,r);
+//             }
+//         }
+
+        
+//     int height(Node* node){
+//         if(node==nullptr) return 0;
+//         return 1 + max(height(node->left), height(node->right));
+//     }
+
+//     bool isBalanced(Node* node){
+//         if(node==nullptr) return true;
+
+//         int lh = height(node->left);
+//         int rh = height(node->right);
+
+//         if(abs(lh-rh)>1)
+//             return false;
+
+//         return isBalanced(node->left) && isBalanced(node->right);
+//     }
+
 
 //         public:
 //             BST(){
@@ -198,22 +242,204 @@ using namespace std;
 //                 inorder(root);
 //                 cout<<endl;
 //             }
+//         }
+
+
+
+
+
+
+
+// class BST{
+//     private:
+//         Node* root;
+
+//         Node* insert(Node* node, int val){
+//             if(node==nullptr){
+//                 return new Node(val);
+//             }
+//             if(val<node->data){
+//                 node->left = insert(node->left, data);
+//             }else{
+//                 node->right = insert(node->right,data);
+//             }
+//             return node;
+//         }
+
+//         void Inorder(){
+//             Inorder(node->left);
+//             cout<<node->data;
+//             Inorder(node->right);
+//         }
+// };
+
+
+
+
+// class Node{
+//     public:
+//         int data;
+//         Node* left;
+//         Node* right;
+
+//         Node(int val){
+//             data = val;
+//             left = nullptr;
+//             right = nullptr;
+//         }
+// };
+
+// class BST{
+//     private:
+//         Node* root;
+
+//         Node* insert(Node* node,int val){
+//             if(node==nullptr){
+//                 return new Node( val);
+//             }
+//             if(node->data>val){
+//                 node->left = insert(node->left,val);
+//             }else{
+//                 node->right = insert(node->right,val);
+//             }
+//             return node;
+//         }
+
+//         void levelorder(Node* node){
+//             if(node==nullptr){
+//                 return;
+//             }
+//             queue<Node*> q;
+//             q.push(node);
+//             while(!q.empty()){
+//                 Node* current = q.front();
+//                 q.pop();
+//                 cout<<current->data;
+//                 if(current->left!=nullptr){
+//                     levelorder(current->left);
+//                 }
+//                 if(current->right!=nullptr){
+//                     levelorder(current->right);
+//                 }
+//             }
+//         }
+// };
+
+
+// void levelorder(Node* node){
+//     if(node==nullptr)return;
+//     queue<Node*> q;
+//     q.push(node);
+//     while(!q.empty()){
+//         Node*current = q.front();
+//         q.pop();
+//         cout<<current->data;
+//         if(current->left!=nullptr){
+//             levelorder(current->left);
+//         }
+//         if(current->right!=nullptr){
+//             levelorder(current->right);
+//         }
+//     }
 // }
 
 
 
+// int main(){
+//     int N,E;
+//     cin>>N>>E;
+//     int u,v,w;
+//     int adjmatrix[N+1][N+1];
+//     for(int i=1;i<=E;i++){
+//         cin>>u>>v>>w;
+//         adjmatrix[u][v]=w;
+//         adjmatrix[v][u]=w;
+//     }
+//     for(int i=1;i<=N;i++){
+//         for(int j=1;j<=N;j++){
+//             cout<<adjmatrix[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
 
-// BST PRINT RANGE
 
-void printRange(Node* node,int L,int R){
-        if(node==nullptr) return;
+//bfs
 
-        if(node->data > L)
-            printRange(node->left,L,R);
 
-        if(node->data >= L && node->data <= R)
-            cout<<node->data<<" ";
+// void bfs(int startNode, vector<vector<int>>& adjList, vector<bool>& visited){
+//     queue<int> q;
+//     q.push(startNode);
+//     visited[startNode]=true;
+//     while(!q.empty()){
+//         int curr = q.front();
+//         q.pop();
+//         cout<<curr<<" ";
+//         for(int i=0;i<adjList.size();i++){
+//             if(adjList[curr][i]==1 && !visited[i]){
+//                 visited[i]=true;
+//                 q.push(i);
+//             }
+//         }
+//         cout<<endl;
+//     }
+// }
 
-        if(node->data < R)
-            printRange(node->right,L,R);
+// int main(){
+//     int n,e;
+//     cin>>n>>e;
+//     vector<vector<int>> adjList (n,vector<int>(n,0));
+//     vector<bool> visited(n,false);
+//     for(int i=0;i<e;i++){
+//         int u,v;
+//         cin>>u>>v;
+//         adjList[u][v]=1;
+//         adjList[v][u]=1;
+//     }
+//     int startNode;
+//     cin>>startnode;
+//     bfs(startNode,adjList,visited);
+// }
+
+
+
+class MinHeap{
+    public:
+        vector<int> arr;
+
+        void HeapifyMax(vector<int>& arr,int n,int i){
+            while(true){
+                int largest = i;
+                int left = 2*i + 1;
+                int right = 2*i + 2;
+
+                if(left<n && arr[left] > arr[largest]){
+                    largest = left;
+                }
+                if(right<n && arr[right] > arr[largest]){
+                    largest = right;
+                }
+                if(largest==i){
+                    break;
+                }
+                swap(arr[i],arr[largest]);
+                i = largest;
+            }
+        }
+};
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    MinHeap heap;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
+    for(int i=n/2-1;i>=0;i--){
+        heap.HeapifyMax(arr,n,i);
+    }
+    for(int i =0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
